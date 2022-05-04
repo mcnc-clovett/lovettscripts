bd7244518cc3a661e619555b5a5f895dd8637aa9
Chris Lovett <clovett@mcnc.org>
2022-05-04 15:38:22 -0400

    Added AD password generator

---
+++ powershell/Generate_Passwords.ps1
@@ -0,0 +1,16 @@
+﻿$users = Get-ADUser -Filter * -Properties GivenName,Surname,EmployeeID
+
+$list = foreach ($u in $users) {
+    if ( $u.GivenName -and $u.Surname -and $u.EmployeeID ) {
+        $givenName = $u.GivenName.Substring(0,1).ToLower()
+        $surname = $(if ($u.Surname.Length -gt 3) {$u.surname.Substring(0,3)} else {$u.surname}).ToLower()
+        $employeeID = $u.EmployeeID.Substring($u.employeeID.Length-4,4)
+
+        [PSCustomObject]@{
+            samAccountName = $u.samAccountname
+            password = "$givenName*$surname#$employeeID!"
+        }
+    }
+}
+
+$list | Export-Csv -NoTypeInformation passwords.csv
